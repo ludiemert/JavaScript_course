7c35783438834830b70087e1e580e8c13314d1d6
Luciana <lucianadiemert@gmail.com>
2023-12-07 19:46:34 -0300

    feat: add exercises Repetiion structure

--- Exercise_02/index.js
+++ Exercise_02/index.js
@@ -1,13 +1,15 @@
 let pilotName = prompt("What your name, Pilot?")
-
 let velocity = 0 
-
 let newVelocity = prompt("What speed would you like to accelerate?")
-
 let confirmVelocity = confirm("we are accelerating  " + newVelocity + "km/s")
-
 if(confirmVelocity) {
   velocity = newVelocity
 }
 
-console.log(velocity)
+if(velocity <= 0) {
+  alert("The ship is stopped, consider and start increasing speed")
+} else {
+     
+}
+
+//console.log(velocity)
---
+++ Exercise_03/index.js
@@ -0,0 +1,19 @@
+/* let velocity = 50
+let acceleration = 5
+
+while(velocity <= 100) {
+console.log("Acceleration: we are: " + velocity + "km/s")
+velocity += acceleration // celocity = velocity + acceleration
+} */
+
+
+let constellation = "Andromeda"
+let = pos = 0
+let constellationLength = constellation.length
+
+while(pos < constellationLength) {
+  if(constellation[pos] == "n" || constellation[pos] == "m") {
+    console.log(pos)
+  }
+  pos += 1
+}
