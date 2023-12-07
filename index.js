90120bc683bb68dca318d3ab8d36f3abf6ab0706
Luciana <lucianadiemert@gmail.com>
2023-12-07 17:23:23 -0300

    feat: initial js exercises

---
+++ index.js
@@ -0,0 +1,13 @@
+let olderPersonName = prompt("Enter the oldest person's name")
+let olderPersonAge = prompt("Enter the oldest person's age")
+
+let youngerPersonName = prompt("Enter the younger person's name")
+let youngerPersonAge = prompt("Enter the younger person's age")
+
+let ageDifference = olderPersonAge - youngerPersonAge
+
+alert (
+         "Person oldest: " + olderPersonName + "\nAge: " + olderPersonAge +
+        "\n\nPerson younger " + youngerPersonName + "\nAge: " + youngerPersonAge +
+        "\n\nDifference the Age: " + ageDifference
+)
