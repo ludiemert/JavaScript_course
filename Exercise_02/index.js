b8445e820ffc724dc7c87a4acf415d36c54270ea
Luciana <lucianadiemert@gmail.com>
2023-12-08 18:14:32 -0300

    feat: add exercises logical operators finish

--- Exercise_02/index.js
+++ Exercise_02/index.js
@@ -1,39 +1,17 @@
 let pilotName = prompt("What your name, Pilot?")
+
 let velocity = 0 
+
 let newVelocity = prompt("What speed would you like to accelerate?")
+
 let confirmVelocity = confirm("we are accelerating  " + newVelocity + "km/s")
+
 if(confirmVelocity) {
-  velocity = newVelocity
-}
-
-/*  Very if \ else    Muitos if \ else
-if(velocity <= 0) {
-  alert("The ship is stopped, consider and start increasing speed")
-} else {
-  if(velocity = 40) {
-    alert(" you are slow we can increase more ")
-  } else {
-    if(velocity < 80) {
-      alert("seems like a good speed to maintain  (parece uma boa velocidade para manter) ")
-    } else {
-      if(velocity < 100) {
-        alert(" Velocidade alta. Considere a diminuir \ High speed. Consider lowering it")
-      } else {
-        alert(" Dangerous speed... forced automatic control. \ Velocidade perigosa... controle automatico forcado...")
-      }
-    }
-  } 
-  
-}
-//console.log(velocity)
-
-*/
-
-  //Improving with fewer ifs using if else / Melhorando com menos if usando if else
+  velocity = newVelocity}
 
   if(velocity <= 0) {
     alert("The ship is stopped, consider and start increasing speed")
-  } else if(velocity = 40) {
+  } else if(velocity < 40) {
       alert(" you are slow we can increase more ")
   } else if(velocity < 80) {
         alert("seems like a good speed to maintain  (parece uma boa velocidade para manter) ")
@@ -43,4 +21,5 @@ if(velocity <= 0) {
      alert(" Dangerous speed... forced automatic control. \ Velocidade perigosa... controle automatico forcado...")
         }
     
+  alert("Pilot: " + pilotName + "\nVelocity:  " + velocity + "km/s")
      
