1ba06e754a5cb96a279347f5e09774adec708a04
Luciana <lucianadiemert@gmail.com>
2023-12-09 14:20:31 -0300

    feat: add Exercises 2.2 =   logical operators - Switch

---
+++ Exercise_02.2/index.js
@@ -0,0 +1,14 @@
+let velocity = 80
+
+switch(velocity * 2) {
+  case 100:
+    console.log("Your velocity is 100km/s")
+    break
+
+  case 160:
+    console.log("Your velocity is 160km/s")
+    break
+
+  case 100:
+    console.log(" Velocity not identify")
+}
